fd0c9b65c0bc21967aedad28e79565dd04ca69a1
Kevin Harrington <kevin.harrington@halodi.com>
2021-08-23 14:15:57 -0400

    adding code

--- manipulator.groovy
+++ manipulator.groovy
@@ -1,1 +1,10 @@
+import eu.mihosoft.vrl.v3d.CSG
+import eu.mihosoft.vrl.v3d.Cylinder
+
 //Your code here
+
+CSG manip = new Cylinder(20,40).toCSG()
+
+manip.getStorage().set("manipulator", "test")
+
+return manip
