bd99c2ae30a437778c4a0f103967f892396f2eae
Kevin Harrington <kevin.harrington@halodi.com>
2021-08-24 15:01:05 -0400

    Adding an object encapsulation of the cartesian moving group

--- manipulator.groovy
+++ manipulator.groovy
@@ -2,6 +2,7 @@ import com.neuronrobotics.bowlerstudio.BowlerStudio
 import com.neuronrobotics.bowlerstudio.physics.TransformFactory
 import com.neuronrobotics.sdk.addons.kinematics.math.RotationNR
 import com.neuronrobotics.sdk.addons.kinematics.math.TransformNR
+import com.neuronrobotics.sdk.common.Log
 
 import eu.mihosoft.vrl.v3d.CSG
 import eu.mihosoft.vrl.v3d.Cylinder
@@ -11,84 +12,121 @@ import javafx.event.EventHandler
 import javafx.event.EventType
 import javafx.scene.input.MouseButton
 import javafx.scene.input.MouseEvent
+import javafx.scene.paint.Color
 import javafx.scene.transform.Affine
 
-Affine manipulationMatrix= new Affine();
+TransformNR p = new TransformNR()
+Affine m= new Affine();
 
-//Your code here
+def event = new Runnable() {
+	public void run() {
+		Log.enableSystemPrint(true)
+		println "New pose found at "+p
+	}
+}
+def eventMoving = new Runnable() {
+	public void run() {
+		Log.enableSystemPrint(true)
+		println "Moving "
+	}
+}
 
-CSG manip = new Cylinder(20,40).toCSG()
-
-
-HashMap<EventType<MouseEvent>,EventHandler<MouseEvent>> map=new HashMap<>()
-double startx=0;
-double starty=0;
-
-double x3d=0;
-double y3d=0;
-double z3d=0;
-double newx=0;
-double newy=0;
-double newz=0;
-Vector3d orintation = new Vector3d(0,1,0);
-TransformNR camFrame=null;
-boolean dragging=false;
-double depth =0;
-
-map.put(MouseEvent.MOUSE_PRESSED,  new EventHandler<MouseEvent>() {
+class manipulation {
+	HashMap<EventType<MouseEvent>,EventHandler<MouseEvent>> map=new HashMap<>()
+	double startx=0;
+	double starty=0;
+	double newx=0;
+	double newy=0;
+	double newz=0;
+	TransformNR camFrame=null;
+	boolean dragging=false;
+	double depth =0;
+	public manipulation(Affine manipulationMatrix,Vector3d orintation,CSG manip,TransformNR globalPose,Runnable eve,Runnable moving) {
+		map.put(MouseEvent.MOUSE_PRESSED,  new EventHandler<MouseEvent>() {
 			@Override
 			public void handle(MouseEvent event) {
-				camFrame= BowlerStudio.getCamerFrame()
-				depth=-1200 /BowlerStudio.getCamerDepth()
-				event.consume()
-				dragging=false;
+				new Thread({
+					camFrame= BowlerStudio.getCamerFrame()
+					depth=-1600 /BowlerStudio.getCamerDepth()
+					event.consume()
+					dragging=false;
+					
+				}).start();
+
 			}
 		})
 
-map.put(MouseEvent.MOUSE_DRAGGED,  new EventHandler<MouseEvent>() {
-	@Override
-	public void handle(MouseEvent event) {
-		if(dragging==false) {
-			startx=event.screenX;
-			starty=event.screenY;
-		}
-		dragging=true;
-		double deltx=(startx-event.screenX);
-		double delty=(starty-event.screenY)
-		TransformNR trans=new TransformNR(deltx / depth,
-							delty / depth, 0, new RotationNR());
-						
-		TransformNR global = camFrame.times(trans);
-		newx=(global.getX()+x3d)*orintation.getX();
-		newy=(global.getY()+y3d)*orintation.getY();
-		newz=(global.getZ()+z3d)*orintation.getZ();
-		global.setX(newx)
-		global.setY(newy)
-		global.setZ(newz)
-		
-		global.setRotation(new RotationNR())
-		Platform.runLater({
-			TransformFactory.nrToAffine(global, manipulationMatrix)
+		map.put(MouseEvent.MOUSE_DRAGGED,  new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				if(dragging==false) {
+					startx=event.screenX;
+					starty=event.screenY;
+				}
+				dragging=true;
+				double deltx=(startx-event.screenX);
+				double delty=(starty-event.screenY)
+				TransformNR trans=new TransformNR(deltx / depth,
+									delty / depth, 0, new RotationNR());
+								
+				TransformNR global = camFrame.times(trans);
+				newx=(global.getX()*orintation.getX()+globalPose.getX());
+				newy=(global.getY()*orintation.getY()+globalPose.getY());
+				newz=(global.getZ()*orintation.getZ()+globalPose.getZ());
+				global.setX(newx)
+				global.setY(newy)
+				global.setZ(newz)
+				
+				global.setRotation(new RotationNR())
+				Platform.runLater({
+					TransformFactory.nrToAffine(global, manipulationMatrix)
+				})
+				double dist = Math.sqrt(Math.pow(deltx, 2)+Math.pow(delty, 2))
+				//System.out.println(" drag "+global.getX()+" , "+global.getY()+" , "+global.getZ()+" "+deltx+" "+delty);
+				moving.run()
+				event.consume()
+			}
 		})
-		double dist = Math.sqrt(Math.pow(deltx, 2)+Math.pow(delty, 2))
-		System.out.println(" drag "+global.getX()+" , "+global.getY()+" , "+global.getZ()+" "+deltx+" "+delty);
 		
-		event.consume()
+		map.put(MouseEvent.MOUSE_RELEASED,  new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				if(dragging) {
+					dragging=false;
+					globalPose.setX(newx)
+					globalPose.setY(newy)
+					globalPose.setZ(newz)
+					event.consume()
+					eve.run()
+				}
+			}
+		})
+		manip.getStorage().set("manipulator",map)
+		manip.setManipulator(manipulationMatrix)
 	}
-})
+	
+}
 
-map.put(MouseEvent.MOUSE_RELEASED,  new EventHandler<MouseEvent>() {
-	@Override
-	public void handle(MouseEvent event) {
-		if(dragging) {
-			dragging=false;
-			z3d=newz
-			y3d=newy
-			x3d=newx
-			event.consume()
-		}
+class CartesianManipulator{
+	CSG manip1 = new Cylinder(0,5,40,10).toCSG()
+			.setColor(Color.BLUE)
+	CSG manip2 = new Cylinder(0,5,40,10).toCSG()
+		.roty(-90)
+		.setColor(Color.RED)
+	CSG manip3 = new Cylinder(0,5,40,10).toCSG()
+		.rotx(90)
+		.setColor(Color.GREEN)
+	public CartesianManipulator(Affine manipulationMatrix,TransformNR globalPose,Runnable ev,Runnable moving) {
+		new manipulation( manipulationMatrix, new Vector3d(0,0,1), manip1, globalPose,ev,moving)
+		new manipulation( manipulationMatrix, new Vector3d(0,1,0), manip3, globalPose,ev,moving)
+		new manipulation( manipulationMatrix, new Vector3d(1,0,0), manip2, globalPose,ev,moving)
 	}
-})
-manip.getStorage().set("manipulator",map)
-manip.setManipulator(manipulationMatrix)
-return manip
+	public ArrayList<CSG> get(){
+		return [manip1,manip2,manip3]
+	}
+}
+
+
+def cart=new CartesianManipulator(m,p,event,eventMoving)
+
+return cart.get()
