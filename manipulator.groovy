4a29ac30ed16df57d4f24f9769777e4491739205
Kevin Harrington <kevin.harrington@halodi.com>
2021-08-26 16:21:17 -0400

    updating path generation

--- manipulator.groovy
+++ manipulator.groovy
@@ -371,7 +371,9 @@ class BezierEditor{
 def URL="https://github.com/madhephaestus/manipulator-test.git"
 def file="bez.json"
 //Temp file
-BezierEditor editor = new BezierEditor(new File(ScriptingEngine.getAppData().getAbsolutePath()+"/bez2.json"),20)
+BezierEditor editor = new BezierEditor(
+	new File(ScriptingEngine.getAppData().getAbsolutePath()+"/bez2.json")
+	,5)
 //Git stored file loaded but not saved
 //BezierEditor editor = new BezierEditor(ScriptingEngine.fileFromGit(URL, file),10)
 //Git file loaded and saved
