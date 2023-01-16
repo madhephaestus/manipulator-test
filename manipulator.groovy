18a307d2b56da3317239de089ec7c32a16127fbc
Kevin Harrington <mad.hephaestus@gmail.com>
2023-01-16 13:02:37 -0500

    updae

--- manipulator.groovy
+++ manipulator.groovy
@@ -257,105 +257,7 @@ class BezierEditor{
 		])
 		bezData.put("number of points",[parts.size()])
 		database.put("bezier",bezData)
-		HashMap<String,Object> msgesData=new HashMap<>();
-		HashMap<String,List<Double>> uuidData=new HashMap<>();
-		uuidData.put("uuid",[
-			0,
-			0,
-			0,
-			0,
-			0,
-			0,
-			0,
-			0,
-			0,
-			0,
-			0,
-			0,
-			0,
-			0,
-			0,
-			0
-		]
-		)
-		msgesData.put("trajectory_id", uuidData)
-		msgesData.put("start_time", [])
-		msgesData.put("balance_mode", [])
-		msgesData.put("append_trajectory", false)
-		HashMap<String,Object> interpolation_modeData=new HashMap<>();
-		interpolation_modeData.put("value",1)
-		msgesData.put("interpolation_mode", interpolation_modeData)
-
-		ArrayList<HashMap<String,Object>>trajectory_points = []
-
-		ArrayList<Transform> transforms = transforms ()
-		for(int i=0;i<parts.size();i++) {
-			TransformNR nr=TransformFactory.csgToNR(transforms.get(i))
-			HashMap<String,Object> pointElement = new HashMap<String,Object>()
-			HashMap<String,Object> time_from_start = new HashMap<String,Object>()
-			ArrayList<HashMap<String,Object>> task_space_commands = new ArrayList<HashMap<String,Object>> ()
-			pointElement.put("joint_space_commands",[])
-			pointElement.put("contact_point_commands",[])
-			pointElement.put("icp_offset_command",[])
-			time_from_start.put("sec", i+1)
-			time_from_start.put("nanosec", 0)
-			pointElement.put("time_from_start", time_from_start)
-			HashMap<String,Object> 	task_space_commandsElement =new HashMap<String,Object> ()
-			HashMap<String,Object> expressed_in_frame=new HashMap<String,Object> ()
-			expressed_in_frame.put("frame_id", 1)
-			task_space_commandsElement.put("expressed_in_frame", expressed_in_frame)
-			HashMap<String,Object> body_frame=new HashMap<String,Object> ()
-			body_frame.put("frame_id", 4)
-			task_space_commandsElement.put("body_frame", body_frame)
-			task_space_commandsElement.put("express_in_z_up", true)
-			HashMap<String,Object> pose=new HashMap<String,Object> ()
-			HashMap<String,Object> posePosition=new HashMap<String,Object> ()
-			HashMap<String,Object> poseOrentation=new HashMap<String,Object> ()
-			posePosition.put("x", nr.getX()/1000.0)
-			posePosition.put("y", nr.getY()/1000.0)
-			posePosition.put("z", nr.getZ()/1000.0)
-
-			poseOrentation.put("x", nr.getRotation().getRotationMatrix2QuaturnionX())
-			poseOrentation.put("y", nr.getRotation().getRotationMatrix2QuaturnionY())
-			poseOrentation.put("z", nr.getRotation().getRotationMatrix2QuaturnionZ())
-			poseOrentation.put("w", nr.getRotation().getRotationMatrix2QuaturnionW())
-			pose.put("position", posePosition)
-			pose.put("orientation", poseOrentation)
-			task_space_commandsElement.put("pose", pose)
-			HashMap<String,Object> angular_velocity=new HashMap<String,Object> ()
-			HashMap<String,Object> linear_velocity=new HashMap<String,Object> ()
-			HashMap<String,Object> angular_acceleration=new HashMap<String,Object> ()
-			HashMap<String,Object> linear_acceleration=new HashMap<String,Object> ()
-			angular_velocity.put("x", 0)
-			angular_velocity.put("y", 0)
-			angular_velocity.put("z", 0)
-			linear_velocity.put("x", 0)
-			linear_velocity.put("y", 0)
-			linear_velocity.put("z", 0)
-			angular_acceleration.put("x", 0)
-			angular_acceleration.put("y", 0)
-			angular_acceleration.put("z", 0)
-			linear_acceleration.put("x", 0)
-			linear_acceleration.put("y", 0)
-			linear_acceleration.put("z", 0)
-			task_space_commandsElement.put("angular_velocity", angular_velocity)
-			task_space_commandsElement.put("linear_velocity", linear_velocity)
-			task_space_commandsElement.put("angular_acceleration", angular_acceleration)
-			task_space_commandsElement.put("linear_acceleration", linear_acceleration)
-			task_space_commandsElement.put("passivity_input", [])
-			task_space_commandsElement.put("orientation_feedback_parameters", [])
-			task_space_commandsElement.put("position_feedback_parameters", [])
-			task_space_commandsElement.put("nullspace_command", [])
-			//			database.put(key,[nr.getX()/1000.0,nr.getY()/1000.0,nr.getZ()/1000.0,
-			//				(nr.getRotation().getRotationAzimuth()),
-			//				(nr.getRotation().getRotationElevation()),
-			//				(nr.getRotation().getRotationTilt())])
-			task_space_commands.add(task_space_commandsElement)
-			pointElement.put("task_space_commands", task_space_commands)
-			trajectory_points.add(pointElement)
-		}
-		msgesData.put("trajectory_points", trajectory_points)
-		database.put("halodi_msgs::msg::dds_::WholeBodyTrajectory_", msgesData)
+	
 		new Thread({
 			println "Saving to file "+cachejson.getAbsolutePath()
 			String writeOut = gson.toJson(database, TT_mapStringString);
@@ -381,12 +283,12 @@ class BezierEditor{
 def URL="https://github.com/madhephaestus/manipulator-test.git"
 def file="bez.json"
 //Temp file
-BezierEditor editor = new BezierEditor(
-		new File(ScriptingEngine.getAppData().getAbsolutePath()+"/bez2.json")
-		,20)
+//BezierEditor editor = new BezierEditor(
+//		new File(ScriptingEngine.getAppData().getAbsolutePath()+"/bez2.json")
+//		,20)
 
 //Git stored file loaded but not saved
-//BezierEditor editor = new BezierEditor(ScriptingEngine.fileFromGit(URL, file),10)
+BezierEditor editor = new BezierEditor(ScriptingEngine.fileFromGit(URL, file),10)
 //Git file loaded and saved
 //BezierEditor editor = new BezierEditor(URL, file,10)
 
